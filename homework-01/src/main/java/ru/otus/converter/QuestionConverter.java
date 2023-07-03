d02c42b5a1c4db9baa6a393ffbff74737e88371c
Ermolaev Evgeniy <eermolaev@rencredit.ru>
2023-07-03 21:46:28 +0300

    fix checkstyle

--- homework-01/src/main/java/ru/otus/converter/QuestionConverter.java
+++ homework-01/src/main/java/ru/otus/converter/QuestionConverter.java
@@ -7,7 +7,7 @@ public class QuestionConverter {
 
 	public String convertToStringWithAnswers(Question question) {
 		StringBuilder builder = new StringBuilder(question.getText()).append(System.lineSeparator());
-		for(Answer answer : question.getAnswers()) {
+		for (Answer answer : question.getAnswers()) {
 			builder.append("\t").append("- ").append(answer.getText()).append(System.lineSeparator());
 		}
 		return builder.toString();
